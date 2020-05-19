45e08342e1e104b4d2dadd108d6cf2a96fd29ba4
Hugo <hmerle14@gmail.com>
2020-05-19 16:23:09 +0200

    First Commit

--- index.php
+++ index.php
@@ -22,11 +22,10 @@
         </div>
     </nav>
     <div>
-        <h3>Welcome. Please register or log in to access website</h3>
+        <h3 class="h3">Welcome. <small class="text-muted">Please register or log in to access website</small></h3>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
-<?php
